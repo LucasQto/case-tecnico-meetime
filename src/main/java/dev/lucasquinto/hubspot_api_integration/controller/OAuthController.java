9c5db60ef2421a28effe0f9df6976666132c1334
Lucas Quinto <lucasquinto22@gmail.com>
2025-04-05 15:22:32 -0300

    feat: implementa endpoint de geração da URL de autorização

---
+++ src/main/java/dev/lucasquinto/hubspot_api_integration/controller/OAuthController.java
@@ -0,0 +1,39 @@
+package dev.lucasquinto.hubspot_api_integration.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import dev.lucasquinto.hubspot_api_integration.model.AuthUrlResponse;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("oauth")
+public class OAuthController {
+
+    @Value("${spring.security.oauth2.client.registration.hubspot.client-id}")
+    private String clientId;
+    @Value("${spring.security.oauth2.client.registration.hubspot.redirect-uri}")
+    private String redirectUri;
+    @Value("${spring.security.oauth2.client.registration.hubspot.scope}")
+    private String scope;
+    @Value("${spring.security.oauth2.client.provider.hubspot.authorization-uri}")
+    private String providerUrl;
+
+    @GetMapping("authorize-url")
+    public ResponseEntity<AuthUrlResponse> genAuthUrl() {
+        String authorizationUrl = UriComponentsBuilder
+        .fromUriString(providerUrl)
+        .queryParam("client_id", clientId)
+        .queryParam("redirect_uri", redirectUri)
+        .queryParam("scope", scope)
+        .queryParam("response_type", "code")
+        .toUriString();
+
+        return ResponseEntity.ok(new AuthUrlResponse(authorizationUrl));
+    } 
+}
---
+++ src/main/java/dev/lucasquinto/hubspot_api_integration/model/AuthUrlResponse.java
@@ -0,0 +1,3 @@
+package dev.lucasquinto.hubspot_api_integration.model;
+
+public record AuthUrlResponse(String authorizationUrl) {}
